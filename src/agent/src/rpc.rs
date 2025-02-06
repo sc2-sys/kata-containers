6b2252ee310e8bb8ce973ab00e3d4bf98ad14c0b
Carlos Segarra <carlos@carlossegarra.com>
2025-02-06 13:31:39 +0000

    sc2(agent): remove copy_file trace call

--- src/agent/src/rpc.rs
+++ src/agent/src/rpc.rs
@@ -1469,10 +1469,11 @@ impl agent_ttrpc::AgentService for AgentService {
 
     async fn copy_file(
         &self,
-        ctx: &TtrpcContext,
+        _ctx: &TtrpcContext,
         req: protocols::agent::CopyFileRequest,
     ) -> ttrpc::Result<Empty> {
-        trace_rpc_call!(ctx, "copy_file", req);
+        // SC2: this trace is too verbose
+        // trace_rpc_call!(ctx, "copy_file", req);
         is_allowed(&req).await?;
 
         do_copy_file(&req).map_ttrpc_err(same)?;
